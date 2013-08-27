98e2f9e3ec769de090dc668f1c56b7de4913a9e7
anupam.chaodhary <anupam.choudhary@mutualmobile.com>
2013-08-27 18:32:51 +0530

    Files Added

---
+++ PracticeProject/PracticeProject/ACNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ACNavigationController.h
+//  PracticeProject
+//
+//  Created by Anupam on 27/08/13.
+//  Copyright (c) 2013 BantuLaLKiDukaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACNavigationController : UINavigationController
+
+@end
---
+++ PracticeProject/PracticeProject/ACNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  ACNavigationController.m
+//  PracticeProject
+//
+//  Created by Anupam on 27/08/13.
+//  Copyright (c) 2013 BantuLaLKiDukaan. All rights reserved.
+//
+
+#import "ACNavigationController.h"
+
+@interface ACNavigationController ()
+
+@end
+
+@implementation ACNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
